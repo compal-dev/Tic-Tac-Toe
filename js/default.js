02390ed8dc1daeb092293ef1dc17fcfdbe7a186e
Develop SD1 <compal_dev@live.com>
2012-03-16 10:36:42 +0800

    fix relative path

---
+++ js/default.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    'use strict';
+    // Uncomment the following line to enable first chance exceptions.
+    // Debug.enableFirstChanceException(true);
+
+
+    WinJS.Application.onmainwindowactivated = function(e) {
+        if (e.detail.kind === Windows.ApplicationModel.Activation.ActivationKind.launch) {
+            // TODO: startup code here
+        }
+
+        
+    }
+    
+    WinJS.Application.start();
+})();
